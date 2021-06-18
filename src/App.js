34e0058f747bda97be0a00502f31e6b7e2c24262
debugger333X <75965411+debugger333X@users.noreply.github.com>
2021-06-18 19:31:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 200px;
+  width: 400px;
+  border-radius: 4px;
+  align-items: center;
+  background-color: teal;
+  font-size: 50px;
+  font-weight: 900;
+  margin-top: 200px;
+  margin-left: 450px;
+  border: none;
+`;
+
+const names = [
+  "debugger333X",
+  "mosh",
+  "eddie",
+  "ben",
+  "ed",
+  "ted",
+  "pie",
+  "benni",
+  "benny",
+  "uqehx",
+  "index",
+  "brow",
+  "bru"
+];
+
+function RandomName() {
+  const [name, newName] = useState("Bob");
+
+  return (
+    <div id="RandomName-container">
+      <Button
+        onClick={() => newName(names[Math.floor(Math.random() * names.length)])}
+      >
+        {name}
+      </Button>
+    </div>
+  );
+}
+
+export default RandomName;
